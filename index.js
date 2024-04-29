fa83b9ca2cc2d62fdf14f40bf85df76b3451358e
Siddiq Sazzad <siddiqsazzad001@gmail.com>
2024-04-29 16:18:31 +0600

    Added Delete Method

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${username}:${password}@mydatabase.rpe1dlh.mongodb.net/?retryWrites=true&w=majority&appName=MyDataBase`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -41,7 +41,20 @@ async function run() {
         const result = await itemCollection.insertOne(newItem);
         res.send(result);
     })
+    
+    app.get('/items/:email', async (req, res) =>{
+      const userEmail = req.params.email;
+      console.log(userEmail)
+      const result =  await itemCollection.find({email:userEmail}).toArray();
+      res.send(result);
+    })
 
+    app.delete('/items/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await itemCollection.deleteOne(query);
+      res.send(result);
+    })
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
