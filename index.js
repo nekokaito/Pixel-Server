34e1dd1205646afa2df4269b2e7a5c4effae5ded
Siddiq Sazzad <siddiqsazzad001@gmail.com>
2024-04-30 00:39:49 +0600

    Pixel Server Home Page

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -28,7 +29,8 @@ async function run() {
     await client.connect();
 
     const itemCollection = client.db('itemDB').collection('item');
-
+    const categoryCollection = client.db('itemDB').collection('subcategory');
+    
     app.post('/items', async (req, res) => {
       const newItem = req.body;
       console.log(newItem);
@@ -47,6 +49,29 @@ async function run() {
     const result = await itemCollection.findOne(query);
     res.send(result);
   })
+  app.put('/items/id/:id', async (req, res) => {
+    const id = req.params.id;
+    const filter = { _id: new ObjectId(id) }
+    const options = { upsert: true };
+    const itemUpdated = req.body;
+
+    const item = {
+        $set: {
+          photo_url: itemUpdated.photo_url,
+          item_name : itemUpdated.item_name,
+          customization: itemUpdated.customization,
+          subcategory_Name: itemUpdated.subcategory_Name,
+          stockStatus: itemUpdated.stockStatus,
+          description: itemUpdated.description,
+          price: itemUpdated.price,
+          processing_time: itemUpdated.processing_time,
+          rating: itemUpdated.rating
+        }
+    }
+
+    const result = await itemCollection.updateOne(filter, item, options);
+    res.send(result);
+})
 
 
 
@@ -54,6 +79,7 @@ async function run() {
       const cursor = itemCollection.find();
       const result = await cursor.toArray();
       res.send(result)
+      
     })
     
     
@@ -63,6 +89,23 @@ async function run() {
       const result = await itemCollection.deleteOne(query);
       res.send(result);
     })
+   
+   
+    app.get('/category', async (req, res) => {
+      const cursor = categoryCollection.find();
+      console.log(cursor)
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+    app.get('/items/category/:url', async (req, res) =>{
+      const Category = req.params.url;
+      const userCategory = Category.replace(/_/g, " ");
+      console.log(userCategory)
+      const result =  await itemCollection.find({subcategory_Name:userCategory}).toArray();
+      res.send(result);
+      console.log(result);
+    })
+
 
    await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -77,9 +120,21 @@ run().catch(console.dir);
 
 
 
-app.get('/', (req, res) =>{
-    res.send("Our Server is Running");
-})
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define route to serve the 'home.html' file
+app.get('/', (req, res) => {
+  const filePath = path.join(__dirname, '/public/home.html');
+  console.log(`Attempting to serve file: ${filePath}`);
+  res.sendFile(filePath, (err) => {
+    if (err) {
+      console.error(`Error serving file: ${err}`);
+      res.status(err.status).end();
+    } else {
+      console.log(`File sent successfully: ${filePath}`);
+    }
+  });
+});
 
 app.listen(port, () =>{
     console.log(port)
